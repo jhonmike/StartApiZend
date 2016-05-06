501a109a4513569176d827b5fa98e254d27bcac9
Jhon Mike <developer@jhonmike.com.br>
2016-05-06 01:33:33 -0300

    upgrade config for zf3

--- config/application.config.php
+++ config/application.config.php
@@ -1,15 +1,16 @@
 <?php
+/**
+ * If you need an environment-specific system or application configuration,
+ * there is an example in the documentation
+ * @see http://framework.zend.com/manual/current/en/tutorials/config.advanced.html#environment-specific-system-configuration
+ * @see http://framework.zend.com/manual/current/en/tutorials/config.advanced.html#environment-specific-application-configuration
+ */
 return [
     // This should be an array of module namespaces used in the application.
     'modules' => [
         'Zend\Router',
         'Zend\Validator',
         'Application',
-//        'DoctrineModule',
-//        'DoctrineORMModule'
-//        'ZfBase',
-//        'ZfAcl',
-//        'ZfUser'
     ],
 
     // These are various options for the listeners attached to the ModuleManager
@@ -65,7 +66,7 @@ return [
     //     ),
     // ),
 
-    // Initial configuration with which to seed the ServiceManager.
-    // Should be compatible with Zend\ServiceManager\Config.
-    // 'service_manager' => array(),
+   // Initial configuration with which to seed the ServiceManager.
+   // Should be compatible with Zend\ServiceManager\Config.
+   // 'service_manager' => array(),
 ];
